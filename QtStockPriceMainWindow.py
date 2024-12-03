182ac5e4129a504441674019f9fae74e57a39fdb
Weichien <foxking0416@gmail.com>
2024-12-03 15:34:21 +0800

    Start

--- QtStockPriceMainWindow.py
+++ QtStockPriceMainWindow.py
@@ -15,29 +15,101 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QHeaderView, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTableView,
-    QWidget)
+from PySide6.QtWidgets import (QApplication, QCheckBox, QDoubleSpinBox, QHBoxLayout,
+    QHeaderView, QMainWindow, QMenuBar, QPushButton,
+    QSizePolicy, QSpacerItem, QStatusBar, QTableView,
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(800, 325)
+        MainWindow.resize(800, 557)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.qtEditPushButton = QPushButton(self.centralwidget)
-        self.qtEditPushButton.setObjectName(u"qtEditPushButton")
-        self.qtEditPushButton.setGeometry(QRect(110, 250, 131, 23))
-        self.qtPriceTableView = QTableView(self.centralwidget)
-        self.qtPriceTableView.setObjectName(u"qtPriceTableView")
-        self.qtPriceTableView.setGeometry(QRect(10, 50, 761, 191))
-        self.qtDeletePushButton = QPushButton(self.centralwidget)
-        self.qtDeletePushButton.setObjectName(u"qtDeletePushButton")
-        self.qtDeletePushButton.setGeometry(QRect(260, 250, 111, 23))
-        self.qtAddPushButton = QPushButton(self.centralwidget)
-        self.qtAddPushButton.setObjectName(u"qtAddPushButton")
-        self.qtAddPushButton.setGeometry(QRect(400, 250, 101, 23))
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.qtDiscountCheckBox = QCheckBox(self.centralwidget)
+        self.qtDiscountCheckBox.setObjectName(u"qtDiscountCheckBox")
+        self.qtDiscountCheckBox.setChecked(True)
+
+        self.horizontalLayout.addWidget(self.qtDiscountCheckBox)
+
+        self.qtDiscountRateDoubleSpinBox = QDoubleSpinBox(self.centralwidget)
+        self.qtDiscountRateDoubleSpinBox.setObjectName(u"qtDiscountRateDoubleSpinBox")
+        self.qtDiscountRateDoubleSpinBox.setEnabled(True)
+        self.qtDiscountRateDoubleSpinBox.setDecimals(1)
+        self.qtDiscountRateDoubleSpinBox.setMaximum(10.000000000000000)
+        self.qtDiscountRateDoubleSpinBox.setSingleStep(0.100000000000000)
+        self.qtDiscountRateDoubleSpinBox.setValue(6.000000000000000)
+
+        self.horizontalLayout.addWidget(self.qtDiscountRateDoubleSpinBox)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer)
+
+        self.qtExtraInsuranceFeeCheckBox = QCheckBox(self.centralwidget)
+        self.qtExtraInsuranceFeeCheckBox.setObjectName(u"qtExtraInsuranceFeeCheckBox")
+
+        self.horizontalLayout.addWidget(self.qtExtraInsuranceFeeCheckBox)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+
+        self.horizontalLayout.addItem(self.horizontalSpacer_2)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.qtStockPositionTableView = QTableView(self.centralwidget)
+        self.qtStockPositionTableView.setObjectName(u"qtStockPositionTableView")
+        self.qtStockPositionTableView.setMinimumSize(QSize(0, 100))
+
+        self.verticalLayout.addWidget(self.qtStockPositionTableView)
+
+        self.qtTradeDataTableView = QTableView(self.centralwidget)
+        self.qtTradeDataTableView.setObjectName(u"qtTradeDataTableView")
+        self.qtTradeDataTableView.setMinimumSize(QSize(0, 300))
+
+        self.verticalLayout.addWidget(self.qtTradeDataTableView)
+
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.qtAddTradingDataPushButton = QPushButton(self.centralwidget)
+        self.qtAddTradingDataPushButton.setObjectName(u"qtAddTradingDataPushButton")
+
+        self.horizontalLayout_3.addWidget(self.qtAddTradingDataPushButton)
+
+        self.qtAddDividendDataPushButton = QPushButton(self.centralwidget)
+        self.qtAddDividendDataPushButton.setObjectName(u"qtAddDividendDataPushButton")
+
+        self.horizontalLayout_3.addWidget(self.qtAddDividendDataPushButton)
+
+        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+
+        self.horizontalLayout_3.addWidget(self.pushButton_2)
+
+        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(u"pushButton")
+
+        self.horizontalLayout_3.addWidget(self.pushButton)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+        self.qtEditDataPushButton = QPushButton(self.centralwidget)
+        self.qtEditDataPushButton.setObjectName(u"qtEditDataPushButton")
+
+        self.verticalLayout.addWidget(self.qtEditDataPushButton)
+
+        self.qtDeleteDataPushButton = QPushButton(self.centralwidget)
+        self.qtDeleteDataPushButton.setObjectName(u"qtDeleteDataPushButton")
+
+        self.verticalLayout.addWidget(self.qtDeleteDataPushButton)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
@@ -54,8 +126,13 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.qtEditPushButton.setText(QCoreApplication.translate("MainWindow", u"\u7de8\u8f2f", None))
-        self.qtDeletePushButton.setText(QCoreApplication.translate("MainWindow", u"\u522a\u9664", None))
-        self.qtAddPushButton.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u589e", None))
+        self.qtDiscountCheckBox.setText(QCoreApplication.translate("MainWindow", u"\u624b\u7e8c\u8cbb\u6298\u6263", None))
+        self.qtExtraInsuranceFeeCheckBox.setText(QCoreApplication.translate("MainWindow", u"\u88dc\u5145\u4fdd\u8cbb", None))
+        self.qtAddTradingDataPushButton.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u589e\u4ea4\u6613\u7d00\u9304", None))
+        self.qtAddDividendDataPushButton.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u589e\u914d\u80a1\u914d\u606f\u7d00\u9304", None))
+        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u589e\u6e1b\u8cc7\u7d00\u9304", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\u65b0\u589e\u589e\u8cc7\u7d00\u9304", None))
+        self.qtEditDataPushButton.setText(QCoreApplication.translate("MainWindow", u"\u7de8\u8f2f", None))
+        self.qtDeleteDataPushButton.setText(QCoreApplication.translate("MainWindow", u"\u522a\u9664", None))
     # retranslateUi
 
--- StockPriceMainWindow.py
+++ StockPriceMainWindow.py
@@ -1,6 +1,164 @@
-
+import sys
+import datetime
+from QtStockPriceMainWindow import Ui_MainWindow  # 導入轉換後的 UI 類
+from QtStockPriceEditDialog import Ui_Dialog
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog
+from enum import Enum
 
 # 要把.ui檔變成.py
 # cd D:\_2.code\PythonStockPrice
 # pyside6-uic QtStockPriceEditDialog.ui -o QtStockPriceEditDialog.py
 # pyside6-uic QtStockPriceMainWindow.ui -o QtStockPriceMainWindow.py
+
+class TradingType( Enum ):
+    BUY = 0
+    SELL = 1
+
+class TradingData( Enum ):
+    TRADING_DATE = 0
+    TRADING_TYPE = 1 # 0:買進, 1:賣出
+    TRADING_PRICE = 2
+    TRADING_COUNT = 3
+    TRADING_DISCOUNT = 4
+
+
+
+class TradingDataDialog( QDialog ):
+    def __init__( self, b_discount, f_discount_value, b_extra_insurance, parent = None ):
+        super().__init__( parent )
+
+        self.ui = Ui_Dialog()
+        self.ui.setupUi( self )
+
+        obj_current_date = datetime.datetime.today()
+        self.ui.qtDateEdit.setDate( obj_current_date.date() )
+        self.ui.qtDateEdit.setCalendarPopup( True )
+        self.ui.qtDiscountCheckBox.setChecked( b_discount )
+        self.ui.qtDiscountRateDoubleSpinBox.setValue( f_discount_value )
+
+        self.ui.qtDiscountCheckBox.stateChanged.connect( self.on_discount_check_box_state_changed )
+        self.ui.qtBuyRadioButton.toggled.connect( self.compute_cost )
+        self.ui.qtSellRadioButton.toggled.connect( self.compute_cost )
+        self.ui.qtCommonTradeRadioButton.toggled.connect( self.on_trading_type_changed )
+        self.ui.qtOddTradeRadioButton.toggled.connect( self.on_trading_type_changed )
+        self.ui.qtPriceDoubleSpinBox.valueChanged.connect( self.compute_cost )
+        self.ui.qtCommonTradeCountSpinBox.valueChanged.connect( self.compute_cost )
+        self.ui.qtOddTradeCountSpinBox.valueChanged.connect( self.compute_cost )
+        self.ui.qtOkButtonBox.accepted.connect( self.accept_data )
+        self.ui.qtOkButtonBox.rejected.connect( self.cancel )
+
+
+        self.dict_trading_data = {}
+
+    def on_discount_check_box_state_changed( self, state ):
+        if state == 2:
+            self.ui.qtDiscountRateDoubleSpinBox.setEnabled( True )
+        else:
+            self.ui.qtDiscountRateDoubleSpinBox.setEnabled( False )
+
+        self.compute_cost()
+
+    def accept_data( self ):
+
+        if float( self.ui.qtTotalCostLineEdit.text() ) != 0:
+            self.dict_trading_data[ TradingData.TRADING_DATE ] = self.ui.qtDateEdit.date().toString( "yyyy-MM-dd" )
+            if self.ui.qtBuyRadioButton.isChecked():
+                self.dict_trading_data[ TradingData.TRADING_TYPE ] = TradingType.BUY
+            else:
+                self.dict_trading_data[ TradingData.TRADING_TYPE ] = TradingType.SELL
+
+            self.dict_trading_data[ TradingData.TRADING_PRICE ] = self.ui.qtPriceDoubleSpinBox.value()
+            if self.ui.qtCommonTradeRadioButton.isChecked():
+                self.dict_trading_data[ TradingData.TRADING_COUNT ] = self.ui.qtCommonTradeCountSpinBox.value() * 1000
+            else:
+                self.dict_trading_data[ TradingData.TRADING_COUNT ] = self.ui.qtOddTradeCountSpinBox.value()
+
+            if self.ui.qtDiscountCheckBox.isChecked():
+                self.dict_trading_data[ TradingData.TRADING_DISCOUNT ] = self.ui.qtDiscountRateDoubleSpinBox.value() / 10
+            else:
+                self.dict_trading_data[ TradingData.TRADING_DISCOUNT ] = 1
+
+            self.accept()
+        else:
+            self.reject()
+    
+    def cancel( self ):
+        print("cancel")
+        self.reject()
+
+    def on_trading_type_changed( self ):
+        if self.ui.qtCommonTradeRadioButton.isChecked():
+            self.ui.qtCommonTradeCountSpinBox.setEnabled( True )
+            self.ui.qtOddTradeCountSpinBox.setEnabled( False )
+        else:
+            self.ui.qtCommonTradeCountSpinBox.setEnabled( False )
+            self.ui.qtOddTradeCountSpinBox.setEnabled( True )
+
+        self.compute_cost()
+
+    def compute_cost( self ):
+        if self.ui.qtCommonTradeRadioButton.isChecked():
+            f_trade_count = self.ui.qtCommonTradeCountSpinBox.value() * 1000
+        else:
+            f_trade_count = self.ui.qtOddTradeCountSpinBox.value()
+        
+        f_trade_value = self.ui.qtPriceDoubleSpinBox.value() * f_trade_count
+        self.ui.qtTradingValueLineEdit.setText( str( f_trade_value ) )
+
+        if self.ui.qtDiscountCheckBox.isChecked():
+            f_discount_value = self.ui.qtDiscountRateDoubleSpinBox.value() / 10
+        else:
+            f_discount_value = 1
+
+        f_trade_fee = int( f_trade_value * 0.001425 * f_discount_value )
+        self.ui.qtFeeLineEdit.setText( str( f_trade_fee ) )
+
+        if self.ui.qtBuyRadioButton.isChecked():
+            f_trade_tax = 0
+            f_total_value = f_trade_value + f_trade_fee
+        else:
+            f_trade_tax = int( f_trade_value * 0.003 )
+            f_total_value = f_trade_value - f_trade_fee - f_trade_tax
+        self.ui.qtTaxLineEdit.setText( str( f_trade_tax ) )
+        self.ui.qtTotalCostLineEdit.setText( str( f_total_value ) )
+
+
+class MainWindow( QMainWindow ):
+    def __init__(self):
+        super( MainWindow, self ).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi( self )  # 設置 UI
+
+        self.ui.qtDiscountCheckBox.stateChanged.connect( self.on_discount_check_box_state_changed )
+        self.ui.qtAddTradingDataPushButton.clicked.connect( self.on_add_new_data_push_button_clicked )
+        self.ui.qtDeleteDataPushButton.clicked.connect( self.on_delete_data_push_button_clicked )
+        self.ui.qtEditDataPushButton.clicked.connect( self.on_edit_data_push_button_clicked )
+
+
+    def on_discount_check_box_state_changed( self, state ):
+        if state == 2:
+            self.ui.qtDiscountRateDoubleSpinBox.setEnabled( True )
+        else:
+            self.ui.qtDiscountRateDoubleSpinBox.setEnabled( False )
+
+    def on_add_new_data_push_button_clicked( self ):
+        dialog = TradingDataDialog( self.ui.qtDiscountCheckBox.isChecked(), self.ui.qtDiscountRateDoubleSpinBox.value(), self.ui.qtExtraInsuranceFeeCheckBox.isChecked(), self )
+
+        if dialog.exec():
+            dict_trading_data = dialog.dict_trading_data
+            pass
+
+    def on_delete_data_push_button_clicked( self ):
+        print("on_delete_data_push_button_clicked")
+
+    def on_edit_data_push_button_clicked( self ):
+        print("on_edit_data_push_button_clicked")
+        # dialog = EditDialog()
+        # dialog.exec()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)  # 創建應用程式
+    app.setStyle('Fusion')
+    window = MainWindow()  # 創建主窗口
+    window.show()  # 顯示窗口
+    sys.exit(app.exec())  # 進入事件循環
